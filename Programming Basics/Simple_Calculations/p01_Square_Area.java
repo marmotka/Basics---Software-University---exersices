676986c44f5381271826643ec7778ac02716e893
Dimana Stefanova <dimanas@gmail.com>
2017-03-23 21:30:00 +0200

    Update and rename p01_Square_Area to p01_Square_Area.java

--- Programming Basics/Simple_Calculations/p01_Square_Area.java
+++ Programming Basics/Simple_Calculations/p01_Square_Area.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
+
 public class p01_Square_Area {
     public static void main(String[] args) {
         System.out.println("Please specify the length of the square a=");
         Scanner console = new Scanner(System.in);
         double a = Double.parseDouble(console.nextLine());
-        double area = a*a;;
+        double area = a * a;
+        ;
         System.out.println("Square = " + area);
 
 
