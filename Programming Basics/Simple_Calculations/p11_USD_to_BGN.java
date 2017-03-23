68a2ec0c713c09b91c9c1ce84ffc816dac69fbde
Dimana Stefanova <dimanas@gmail.com>
2017-03-23 21:36:57 +0200

    Create p11_USD_to_BGN.java

---
+++ Programming Basics/Simple_Calculations/p11_USD_to_BGN.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class p11_USD_to_BGN {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        double usd = Double.parseDouble(console.nextLine());
+        double bgn = usd * 1.79549;
+        System.out.printf("%.2f", bgn);
+    }
+}
