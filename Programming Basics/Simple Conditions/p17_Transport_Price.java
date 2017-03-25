370afb394ec6659d485b56f4812fade7452a0bf7
Dimana Stefanova <dimanas@gmail.com>
2017-03-25 15:32:41 +0200

    Update p17_Transport_Price.java

--- Programming Basics/Simple Conditions/p17_Transport_Price.java
+++ Programming Basics/Simple Conditions/p17_Transport_Price.java
@@ -1,31 +1,30 @@
 import java.util.Scanner;
 
-public class p17_Transport_Price {
+public class p17_Transport_Price_OK {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
-        boolean time = "day".equals(scanner.nextLine());
+        String time = scanner.nextLine();
 
         double prTa;
         double prBus = 0.09 * n;
         double prTr = 0.06 * n;
 
-        if (time) {
-            prTa = 0.7 + 0.79 * n;
-        } else {
-            prTa = 0.7 + 0.9 * n;
-        }
 
         if (n >= 100) {
-            System.out.printf("%.2f", Math.min(Math.min(prTa, prBus), prTr));
+            System.out.printf("%.2f", prTr);
         } else if (n >= 20) {
-            System.out.printf("%.2f", Math.min(prTa, prBus));
+            System.out.printf("%.2f", prBus);
         } else {
-            System.out.printf("%.2f", prTa);
+            if (time.equals("day")) {
+                prTa = 0.7 + 0.79 * n;
+                System.out.printf("%.2f", prTa);
+            } else if (time.equals("night")) {
+                prTa = 0.7 + 0.9 * n;
+                System.out.printf("%.2f", prTa);
+            }
         }
-
-
     }
 }
